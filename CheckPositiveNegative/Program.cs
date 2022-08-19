c725e1091ab9575bf9fb7164f6830c57bc24cafc
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-19 16:27:39 +0530

    CheckPositiveNegative

---
+++ CheckPositiveNegative/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            System.Console.Write("Enter any number: ");
+            num = Convert.ToInt32(System.Console.ReadLine());
+            if (num >= 0)
+                System.Console.Write("{0} is positive number", num);
+            else
+                System.Console.Write("{0} is negative number", num);
+            System.Console.ReadLine();
+        }
+    }
+}
