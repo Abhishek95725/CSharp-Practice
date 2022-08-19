e4a2888666843d4315d26cb84bc59a1da4da5e98
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-19 16:43:49 +0530

    CheckHighestNumber
    Check Largest Number by user input

---
+++ CheckHighestNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckHighestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, high = 0, num = 0;
+            while (x < 5)
+            {
+                System.Console.Write("Enter any number: ");
+                num = Convert.ToInt32(System.Console.ReadLine());
+                if (num > high)
+                    high = num;
+                x = x + 1;
+            }
+                    System.Console.WriteLine("High number is: "+high);
+            System.Console.ReadLine();
+            }
+        }
+    }
