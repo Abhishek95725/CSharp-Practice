04cbf9dfd5eece8acb6f5c86e7398f8aa54f9812
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-24 02:52:36 +0530

    ArrayExample
    Array Program in all loop (for, do, while, foreach)

---
+++ ArrayExample/ArrayExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("================ Do Loop ===================");
+            /*  int []arr = {101,102,103,104,105};
+              int i = 0;
+              do
+              {
+                  Console.WriteLine(arr[i]);
+                  i = i + 1;
+              } while (i < 5);
+              Console.ReadLine(); */
+            //    Console.WriteLine("================ While Loop ===================");
+            /*
+             int i = 0;
+             int []arr;
+             arr = new int[5];
+             arr[0] = 8;
+             arr[1] = 12;
+             arr[2] = 18;
+             arr[3] = 25;
+             arr[4] = 29;
+             while(i<5)
+             {
+                 Console.WriteLine(arr[i]);
+                 i = i + 1;
+             }
+             Console.ReadLine(); 
+             */
+            //  Console.WriteLine("=============== For Loop ====================");
+            /*
+              int[] arr = { 108, 102, 109, 124, 148 };
+              for(int i=0; i<5; i++)
+              {
+                  Console.WriteLine(arr[i]);
+              }
+              Console.ReadLine();
+              */
+            Console.WriteLine("================ foreach Loop ===================");
+            int i = 0;
+            int[] arr = { 108, 102, 109, 124, 148 };
+            foreach(int a in arr)
+            {
+                Console.WriteLine(arr[i]);
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
