e9085ba7374b7f4d8deb872b6d89eb3b205c8d93
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-26 02:33:08 +0530

    ReverseNumberANDCountEvenOddWithTheHelpOfMethod
    Reverse Number AND Count Even Odd With The Help Of Method

---
+++ ArrayExample/ReverseNumberANDCountEvenOdd/ReverseNumberANDCountEvenOdd/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumberANDCountEvenOdd
+{
+    internal class Class1
+    {
+        void CountEvenOdd(int[] arr)
+        {
+            int even = 0;
+            int odd = 0;
+            Console.Write("My Array: ");
+                for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+            Console.WriteLine();
+            for(int j = 0; j < arr.Length; j++)
+            {
+                if (arr[j]%2==0)
+                {
+                    Console.WriteLine("Even number is: " + arr[j]);
+                    even++;
+                }
+                else
+                {
+                    Console.WriteLine("Odd number is: " + arr[j]);
+                    odd++;
+                }
+            }
+            Console.WriteLine("Total Even Number is: "+even);
+            Console.WriteLine("Total Odd Number is: " +odd);
+        }
+        public static void Main(string[] args)
+        {
+            int[] ar = new int[5];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write("Enter the value of " + (i + 1) + " Array :");
+                ar[i] = int.Parse(Console.ReadLine());
+            }
+            Class1 obj = new Class1();
+            obj.CountEvenOdd(ar);
+        }
+    }
+}
