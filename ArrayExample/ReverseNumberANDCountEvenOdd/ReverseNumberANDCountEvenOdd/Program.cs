f763b947d3448a02b3b92871161a2f4c5e561b5e
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-26 00:09:40 +0530

    ReverseNumberANDCountEvenOdd
    Reverse Number AND Count Even or Odd

---
+++ ArrayExample/ReverseNumberANDCountEvenOdd/ReverseNumberANDCountEvenOdd/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumberANDCountEvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int even = 0, odd = 0;
+            Console.Write("Enter size of array: ");
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+            int k = 0;
+            while (k < num)
+            {
+                Console.Write("Enter number: ");
+                arr[k] = int.Parse(Console.ReadLine());
+                k++;
+            }
+            int i = num-1;
+            while (i >= 0)
+            {
+                Console.WriteLine(arr[i]);
+                int j = 0;
+                while (j < 1)
+                {
+                    if (arr[i] % 2 == 0)
+                        even++;
+                    else
+                        odd++;
+                    j++;
+                }
+                i--;
+            }
+                Console.WriteLine("Even Number: "+even);
+                Console.WriteLine("Odd Number: " +odd);
+                Console.ReadLine();
+            }
+        }
+    }
+
