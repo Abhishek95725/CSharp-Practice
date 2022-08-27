8e2e8094accbaacb72dab90b8efefeb7f38e555a
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-27 17:35:34 +0530

    MinValueArray
    Min Value Array using While Loop

---
+++ ArrayExample/MinValueArray/MinValueArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinValueArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               Console.Write("Enter the size of array: ");
+                int num = int.Parse(Console.ReadLine());
+                int[] arr = new int[num];
+                int min = int.MaxValue;
+                int i = 0;
+                while (i < num)
+                {
+                    Console.Write("Enter the number: ");
+                    arr[i] = int.Parse(Console.ReadLine());
+                    if (arr[i] < min)
+                        min = arr[i];
+                    i++;
+                }
+                Console.WriteLine("Min value: " + min);
+                Console.ReadLine();
+            }
+        }
+    }
