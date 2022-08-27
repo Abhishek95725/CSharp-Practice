d5b5e3814e0bc18589b36d6a344503833a72bd3e
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-28 01:28:16 +0530

    CountPrimeArray
    Count Prime Array - While

---
+++ ArrayExample/CountPrimeArray/CountPrimeArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountPrimeArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of array: ");
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+            int prime=0;
+            int notprime = 0;
+            int i, j;
+            i = 0;
+            while(i<num)
+            {
+                Console.Write("Enter element of array: ");
+                arr[i] = int.Parse(Console.ReadLine());
+                int count = 0;
+                 j = 1;
+                while (j <= arr[i])
+                {
+                    if (arr[i] % j == 0)
+                        count++;
+                    j++;
+                }
+                      if  (count == 2)
+                    prime++;
+                else
+                            notprime++;
+                i++;
+            }
+            Console.WriteLine("count prime number is: " + prime);
+            Console.WriteLine("count not prime number is: " + notprime);
+            Console.ReadLine();
+        }
+    }
+}
