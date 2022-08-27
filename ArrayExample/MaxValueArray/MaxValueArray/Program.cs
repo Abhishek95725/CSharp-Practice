b1dc3f2df38f3ae57eb31b34c0d879f01280b4da
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-27 15:39:00 +0530

    Array - Max Value using While Value
    Array - Max Value using While Value

---
+++ ArrayExample/MaxValueArray/MaxValueArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxValueArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of Array: ");
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+            int max = 0;
+            int i = 0;
+            while(i<num)
+            {
+                Console.Write("Enter the number: ");
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] > max)
+                    max = arr[i];
+                i++;
+            }
+            Console.WriteLine("Max value is: "+max);
+            Console.ReadLine();
+        }
+    }
+}
