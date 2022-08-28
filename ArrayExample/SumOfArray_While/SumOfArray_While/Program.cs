d4655c7c230ae1e67c57096d7b0b0d07e36cb0e3
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-28 23:38:59 +0530

    SumOfArray_While
    SumOfArray_While

---
+++ ArrayExample/SumOfArray_While/SumOfArray_While/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfArray_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of array: ");
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+            int i = 0;
+            int sum = 0;
+            while (i < num)
+            {
+                Console.Write("Enter the element of array: ");
+                arr[i] = int.Parse(Console.ReadLine());
+                sum = sum + arr[i];
+                i++;
+            }
+            Console.WriteLine("Sum of array: "+sum);
+            Console.ReadLine();
+        }
+    }
+}
