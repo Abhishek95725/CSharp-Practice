af134df2408f50c227db9e37a5d714fcf5280579
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-31 02:30:47 +0530

    Pattern_Program
    Pattern_Program 
    
    A A A A A
    A A A A A
    A A A A A
    A A A A A
    A A A A A

---
+++ All_Pattern_Program/Pattern_Program/Pattern_Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j;
+            while (i < 5)
+            {
+                j = 1;
+                while (j < 6)
+                {
+                    Console.Write("A" + " ");
+                    j = j + 1;
+                }
+                Console.WriteLine();
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
