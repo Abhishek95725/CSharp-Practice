95275f3218d5b352e468fbcf018290cf1a52d7b5
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-09-03 21:23:36 +0530

    Convert_Num_2_String
    Convert_Num_2_String

---
+++ Convert_Num_2_String/Convert_Num_2_String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Num_2_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int num = 95;
+            s = num.ToString();
+            Console.WriteLine("String is: "+s);
+            Console.ReadLine();
+        }
+    }
+}
